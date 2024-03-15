b5fc8e447db9e780107ce566825cb25d15b38bc9
abhisekkatual11 <abhisekkatual565@gmail.com>
2024-03-15 20:53:48 +0530

    first commit

---
+++ src/main/java/com/chin2/it/GitHubApplication.java
@@ -0,0 +1,13 @@
+package com.chin2.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chin2/it/GitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chin2.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
